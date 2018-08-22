065f5b2e173632c65c27a5b293f61d614e7b255c
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-21 19:27:21 -0700

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-set LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%
+set LIBRARY_DIRS=%LIBRARY_LIB%
 set INCLUDE_DIRS=%LIBRARY_INC%
 
 "%PYTHON%" -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
