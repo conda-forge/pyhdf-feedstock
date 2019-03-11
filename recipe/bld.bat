28fbf3f98a98684368303f939379fff685223592
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-11 14:21:27 -0300

    I 8 quotes on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-set LIBRARY_DIRS="%LIBRARY_BIN%;%LIBRARY_LIB%"
-set INCLUDE_DIRS="%LIBRARY_INC%"
+set "LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%"
+set "INCLUDE_DIRS=%LIBRARY_INC%"
 
 :: We set the version in the pin file but the build expects this to be a directory.
 :: we should wither patch it or send a PR upstream to avoid this workaround.
