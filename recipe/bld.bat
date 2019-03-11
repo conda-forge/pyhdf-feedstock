9b7e7bff66db9664660979e26799e21776924466
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-11 08:28:26 -0300

    try patching and search in libbin

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,4 @@
+set LIBRARY_DIRS="%LIBRARY_BIN%;%LIBRARY_LIB%"
 set INCLUDE_DIRS="%LIBRARY_INC%"
-set LIBRARY_DIRS="%LIBRARY_LIB%"
-set HDF4="%LIBRARY_PREFIX%"
 
 %PYTHON% -m pip install . --no-deps -vv
