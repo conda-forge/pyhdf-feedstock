0d02fcbbd92f9c900f210da11466f398c695e2bb
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-11 09:18:59 -0300

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 set LIBRARY_DIRS="%LIBRARY_BIN%;%LIBRARY_LIB%"
 set INCLUDE_DIRS="%LIBRARY_INC%"
 
-%PYTHON% -m pip install . --no-deps -vv
+REM %PYTHON% -m pip install . --no-deps -vv
+%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
