f73fcc2d48a22a95fda9593461289a33e6cab150
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-11 16:16:21 -0300

    let's see if I can get a better error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,9 @@
-set LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%
-set INCLUDE_DIRS=%LIBRARY_INC%
-
 :: We set the version in the pin file but the build expects this to be a directory.
 :: we should wither patch it or send a PR upstream to avoid this workaround.
 set hdf4=
-%PYTHON% -m pip install . --no-deps -vv
+
+set LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%
+set INCLUDE_DIRS=%LIBRARY_INC%
+
+REM %PYTHON% -m pip install . --no-deps -vv
+%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
